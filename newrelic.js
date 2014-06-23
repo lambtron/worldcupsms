4fe2a82b1b400952ba9bba228b01b6ed429de30c
Andy Jiang <andyjiang@gmail.com>
2014-06-22 22:01:08 -0700

    moved new relic license key to config

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : '9e0edaf1138bcad04175d9b423080b52a86bcdf8',
+  license_key : process.env.NEW_RELIC_LICENSE_KEY,
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
