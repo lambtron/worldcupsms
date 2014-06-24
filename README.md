3969f4a2e4ae8ff1a4c3ce3aadf054a4abbcb93d
Andy Jiang <andyjiang@gmail.com>
2014-06-23 22:51:22 -0700

    updated readme

--- README.md
+++ README.md
@@ -3,4 +3,40 @@
 App is here:
 http://worldcupsms.herokuapp.com/
 
-Send a text to (415) 799-2563 to subscribe to FIFA World Cup notifications!
+[Link](http://worldcupsms.herokuapp.com) "FIFA World Cup Alerts") to the FIFA world cup alerts.
+
+Subscribe to FIFA world cup events by texting **(415) 799-2563**.
+
+Text **GOAL** to receive all goals, and the start and end of matches; **CARD** to receive all goals, red / yellow cards, and the start and end of matches; and **SUB** to receive everything (goals, cards, substitions, and the start and end of matches). You can send **STOP** to discontinue SMS updates. Or follow [@fifaWCalerts](http://www.twitter.com/fifawcalerts) for tweet updates.
+
+# Running it locally
+
+You need
+- npm
+- node
+- mongodb
+- [Twilio](http://www.twilio.com)
+- [Twitter](http://www.twitter.com) (get an account and then register an app)
+
+First, clone the project. Then, add a `./config/config.js` file that looks like this.
+
+```javascript
+process.env.TWILIO_ASID = 'Axxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+process.env.TWILIO_AUTH_TOKEN = 'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+process.env.TWILIO_PHONE_NUMBER = '+14158888888';
+process.env.TWIT_CONSUMER_KEY = 'MXXxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+process.env.TWIT_CONSUMER_SECRET = 'uxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+process.env.TWIT_ACCESS_TOKEN = '212311534-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+process.env.TWIT_ACCESS_TOKEN_SECRET = 'FExxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+```
+
+Execute the following commands in your terminal.
+```shell
+$ npm install
+$ mongod
+$ node server.js
+```
+
+Any questions or comments, just tweet at me @andyjiang.
+
+Happy coding!
--- config/routes.js
+++ config/routes.js
@@ -2,132 +2,133 @@
 
 (function() {
 
-/**
- * Import helpers ==============================================================
- */
-var Twilio = require('../app/helpers/twilio');
-var Worldcup = require('../app/helpers/worldcup');
-var Twit = require('twit');
+  /**
+   * Import helpers ==============================================================
+   */
+  var Twilio = require('../app/helpers/twilio');
+  var Worldcup = require('../app/helpers/worldcup');
+  var Twit = require('twit');
 
-var T = new Twit({
-    consumer_key: process.env.TWIT_CONSUMER_KEY
-  , consumer_secret: process.env.TWIT_CONSUMER_SECRET
-  , access_token: process.env.TWIT_ACCESS_TOKEN
-  , access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET
-});
+  var T = new Twit({
+      consumer_key: process.env.TWIT_CONSUMER_KEY
+    , consumer_secret: process.env.TWIT_CONSUMER_SECRET
+    , access_token: process.env.TWIT_ACCESS_TOKEN
+    , access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET
+  });
 
-/**
- * Import models ===============================================================
- */
-var User = require('../app/models/user');
+  /**
+   * Import models ===============================================================
+   */
+  var User = require('../app/models/user');
 
-var TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+  var TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
 
-// Public functions. ===========================================================
-module.exports = function (app) {
-  // Retrieve data from WorldCup endponit every minute.
-  // If new event, then send POST request to phone numbers.
-  var pingAPI = function pingAPI () {
-    Worldcup.getEvents( function (err, data) {
-      if (data.length > 0) {
-        // Tweet it.
-        var status = data + ' #worldcup';
-        T.post('statuses/update', { status: status },
-          function (err, data, response) {
-            // Success.
-        });
+  // Public functions. ===========================================================
+  module.exports = function (app) {
+    // Retrieve data from WorldCup endponit every minute.
+    // If new event, then send POST request to phone numbers.
+    var pingAPI = function pingAPI () {
+      Worldcup.getEvents( function (err, data) {
+        if (data.length > 0) {
+          // Tweet it.
+          var status = data + ' #worldcup';
+          T.post('statuses/update', { status: status },
+            function (err, data, response) {
+              // Success.
+          });
 
-        // Remove hashtags for texts.
-        if (data.indexOf('#') > 0)
-          data = data.substring(0, data.indexOf('#')).trim();
+          // Remove hashtags for texts.
+          if (data.indexOf('#') > 0)
+            data = data.substring(0, data.indexOf('#')).trim();
 
-        // Send texts to those appropriate.
-        User.create.find({}).exec(function (err, users) {
-          for (var i = 0; i < users.length; i ++) {
-            (function (i, event) {
-              var type = users[i].type;
-              if (!type)
-                type = 'goal';
+          // Send texts to those appropriate.
+          User.create.find({}).exec(function (err, users) {
+            for (var i = 0; i < users.length; i ++) {
+              (function (i, event) {
+                var type = users[i].type;
+                if (!type)
+                  type = 'goal';
 
-              if (type == 'sub') {
-                Twilio.sendMessage(users[i].phone_number, TWILIO_PHONE_NUMBER,
-                  event);
-              } else if (type == 'card' &&
-                (event.indexOf('card') > 0 || event.indexOf('goal') > 0 ||
-                event.indexOf('game') > 0)) {
-                Twilio.sendMessage(users[i].phone_number, TWILIO_PHONE_NUMBER,
-                  event);
-              } else if (event.indexOf('goal') > 0 || event.indexOf('game') > 0) {
-                Twilio.sendMessage(users[i].phone_number, TWILIO_PHONE_NUMBER,
-                  event);
-              }
-            })(i, this);
-          }
-        }.bind(data));
-      };
-    });
-  };
-
-  pingAPI();
-  var interval = setInterval( function() {
-    pingAPI();
-  }, 6000);
-
-	// Application routes ========================================================
-  app.post('/new', function (req, res) {
-    // New person texting us.
-    var phone_number = Twilio.standardizePhoneNumber(req.body.From);
-    var type = ""; // goals, cards, subs
-    var body = req.body.Body.toLowerCase();
-
-    if (body.indexOf('sub') > -1)
-      type = "sub";
-    else if (body.indexOf('card') > -1)
-      type = "card";
-    else
-      type = "goal";
-
-    var load = {
-      phone_number: phone_number,
-      type: type
+                if (type == 'sub') {
+                  Twilio.sendMessage(users[i].phone_number, TWILIO_PHONE_NUMBER,
+                    event);
+                } else if (type == 'card' &&
+                  (event.indexOf('card') > 0 || event.indexOf('goal') > 0 ||
+                  event.indexOf('game') > 0)) {
+                  Twilio.sendMessage(users[i].phone_number, TWILIO_PHONE_NUMBER,
+                    event);
+                } else if (event.indexOf('goal') > 0 ||
+                  event.indexOf('game') > 0) {
+                  Twilio.sendMessage(users[i].phone_number, TWILIO_PHONE_NUMBER,
+                    event);
+                }
+              })(i, this);
+            }
+          }.bind(data));
+        };
+      });
     };
 
-    // Add to mongoDB.
-    User.create.find({phone_number: phone_number}).exec(function (err, users) {
-      if (users.length > 0) {
-        // Delete all duplicates.
-        for (var i = 0; i < users.length - 1; i++) {
-          (function (user) {
-            user.remove();
-          })(users[i]);
+    pingAPI();
+    var interval = setInterval( function() {
+      pingAPI();
+    }, 6000);
+
+  	// Application routes ========================================================
+    app.post('/new', function (req, res) {
+      // New person texting us.
+      var phone_number = Twilio.standardizePhoneNumber(req.body.From);
+      var type = ""; // goals, cards, subs
+      var body = req.body.Body.toLowerCase();
+
+      if (body.indexOf('sub') > -1)
+        type = "sub";
+      else if (body.indexOf('card') > -1)
+        type = "card";
+      else
+        type = "goal";
+
+      var load = {
+        phone_number: phone_number,
+        type: type
+      };
+
+      // Add to mongoDB.
+      User.create.find({phone_number: phone_number}).exec(function (err, users) {
+        if (users.length > 0) {
+          // Delete all duplicates.
+          for (var i = 0; i < users.length - 1; i++) {
+            (function (user) {
+              user.remove();
+            })(users[i]);
+          }
+
+          // Exists, so just update.
+          User.upsertUser(this.phone_number, this.type, function (err, records) {
+            var msg = "You have updated your subscription settings to '" +
+              this.type + "\'.";
+            Twilio.sendMessage(this.phone_number, TWILIO_PHONE_NUMBER, msg);
+          }.bind(this));
+        } else {
+          // Add new.
+          User.create.create({
+            phone_number: phone_number,
+            type: type
+          }, function(err, user) {
+            // Success.
+            var welcome = "Thanks for subscribing to FIFA World Cup 2014 text" +
+              " updates. At anytime you wish to end, just respond with STOP.";
+            Twilio.sendMessage(user.phone_number, TWILIO_PHONE_NUMBER, welcome);
+          });
         }
+      }.bind(load));
 
-        // Exists, so just update.
-        User.upsertUser(this.phone_number, this.type, function (err, records) {
-          var msg = "You have updated your subscription settings to '" +
-            this.type + "\'.";
-          Twilio.sendMessage(this.phone_number, TWILIO_PHONE_NUMBER, msg);
-        }.bind(this));
-      } else {
-        // Add new.
-        User.create.create({
-          phone_number: phone_number,
-          type: type
-        }, function(err, user) {
-          // Success.
-          var welcome = "Thanks for subscribing to FIFA World Cup 2014 text" +
-            " updates. At anytime you wish to end, just respond with STOP.";
-          Twilio.sendMessage(user.phone_number, TWILIO_PHONE_NUMBER, welcome);
-        });
-      }
-    }.bind(load));
+      res.send(200);
+    });
 
-    res.send(200);
-  });
-
-	app.get('/', function (req, res) {
-    res.sendfile('index.html', {'root': './public/views/'});
-  });
-};
+  	app.get('/*', function (req, res) {
+      res.sendfile('index.html', {'root': './public/views/'});
+    });
+  };
 
 }());
