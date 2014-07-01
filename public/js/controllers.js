0cbb3c992228b9da90fe3719bea4171046bddfc3
Andy Jiang <andyjiang@gmail.com>
2014-06-30 21:50:41 -0700

    now ajax to get number

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,17 +5,29 @@ worldcup.controller('mainController',
 	function ($scope, $http)
 {
 	// Initialize.
-	var number = $scope.number = '';
+	$scope.number = '';
 
 	// Get the number.
 	$http.get('/number')
 	.success(function (data) {
-		console.log('Success.');
-		console.log(data);
-		number = data.number;
+		$scope.number = beautifyTelNum(data.number);
 	})
 	.error(function (data) {
-		console.log('Error.');
+		console.log('Error:');
 		console.log(data);
 	});
+
+	function beautifyTelNum (num) {
+		// num is '+12409887758'
+		// remove the first two characters
+		// put '(' first
+		// put ') ' after
+		// put '-' after
+		var arr = num.split('');
+		arr.splice(0, 2);	// remove '+1'
+		arr.unshift('(');
+		arr.splice(4, 0, ') ');
+		arr.splice(8, 0, '-');
+		return arr.join('');
+	}
 }]);
